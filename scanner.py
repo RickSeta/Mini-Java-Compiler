4cf6944e62c0e80c2324cd34e49ec2a893b39090
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-12-10 16:38:42 -0300

    comentarios funcionando

--- scanner.py
+++ scanner.py
@@ -1,15 +1,14 @@
 import numpy as np
 
+
+
+
+
 def is_space(char):
     return char in {' ', '\n', '\t', '\r', '\f'}
 
-Input = list("if (x == 1) { System.out.println(x); } else { System.out.println(y); }")
-
-
-
-
 def check_special_char(char,token):
-    is_special = char in {'+', '-', '*', '/', '(', ')', '{', '}', '[', ']', ';', ',', '.', '=', '!', '<', '>', '&'}
+    is_special = char in {'+', '-', '*', '(', ')', '{', '}', '[', ']', ';', ',', '.', '=', '!', '<', '>', '&'}
     # print("Caractere passado: ", char)
     if is_special:
         if char == '.' and (token == "System" or token == "System.out"):
@@ -34,6 +33,7 @@ def checa_aceito(state, accept, token, token_sequence):
         return
 
     if (accept[state]):
+        print(token, state)
         if reservada(token):
             print("\nPalavra reservada: ", token)
             token_sequence.append((token, token))
@@ -55,13 +55,6 @@ def checa_aceito(state, accept, token, token_sequence):
     else:
         print("Rejeitado\n")
 
-def checa_comentario(token):
-    if token == '/*':
-        return 1
-    elif token == '*/':
-        return 0
-    elif token == '//':
-        return 2
 
 def check_char(char):
     if char.isalpha():
@@ -72,69 +65,77 @@ def check_char(char):
         return 'underline'
     if char == ".": 
         return '.'
+    if char == "/":
+        return '/'
+    if char == "*":
+        return '*'
+    if char == "\n":
+        return '\n'
     return 'outro'
 
-def scanner(input ): 
-
+def scanner(input): 
+    input.append(' ')
     transitionTable = {
-    1: {'letra': 2, 'numero': 4, 'underline': 3, 'outro': 3, '.' : 3}, # Estado inicial
-    2: {'letra': 2, 'numero': 2, 'underline': 2, 'outro': 3, '.' : 5}, # Estado de aceitação identificador
-    3: {'letra': 3, 'numero': 3, 'underline': 3, 'outro': 3, '.' : 3}, # Estado de rejeição
-    4: {'letra': 3, 'numero': 4, 'underline': 3, 'outro': 3, '.' : 3}, # Estado para numeros
-    5: {'letra': 5, 'numero': 3, 'underline': 3, 'outro': 3, '.' : 5}, # Estado para o System.out.println
-    }
-    accept = {1: False, 2: True, 3: False, 4: True, 5: True}
-    
-    tipo_comentario = 0
-    input_current = 0
-    final_index= len(input)
-    token_sequence = []
+    1: {'letra': 2, 'numero': 4, 'underline': 3, 'outro': 3, '.' : 3, '/': 6, '*': 3, '\n': 2 }, # Estado inicial
+    2: {'letra': 2, 'numero': 2, 'underline': 2, 'outro': 3, '.' : 5, '/': 6, '*': 3, '\n': 2 }, # Estado de aceitação identificador
+    3: {'letra': 3, 'numero': 3, 'underline': 3, 'outro': 3, '.' : 3, '/': 6, '*': 3, '\n': 3 }, # Estado de erro
+    4: {'letra': 3, 'numero': 4, 'underline': 3, 'outro': 3, '.' : 3, '/': 6, '*': 3, '\n': 3 }, # Estado para numeros
+    5: {'letra': 5, 'numero': 3, 'underline': 3, 'outro': 3, '.' : 5, '/': 6, '*': 3, '\n': 5 }, # Estado para o System.out.println
 
-    while(input_current < final_index):
+    6: {'letra': 3, 'numero': 3, 'underline': 3, 'outro': 3, '.' : 3, '/': 7, '*': 9, '\n': 3 }, # Estado para o segundo char do indicador de comentario
+    7: {'letra': 7, 'numero': 7, 'underline': 7, 'outro': 7, '.' : 7, '/': 7, '*': 7, '\n': 1 }, # Estado para comentario inline
+
+    9: {'letra': 9, 'numero': 9, 'underline': 9, 'outro': 9, '.' : 9, '/': 9, '*': 10,'\n': 9 }, # Estado para primeiro asterisco do block comment
+    10: {'letra': 9,'numero': 9, 'underline': 9, 'outro': 9, '.' : 9, '/': 1, '*': 10,'\n': 9 }, # Estado para asterisco dentro do block comment aguardadndo pela barra
+    }
+    accept = {1: False, 2: True, 3: False, 4: True, 5: True, 6: False, 7: True, 8: True, 9: False, 10: False}
+    
+    token_sequence = []
+    not_comment = True
+
+    while(len(input) > 0):
         token = ""
         state = 1
-        while(input_current < final_index):
-
+        while(len(input) > 0):
             ch = input.pop(0)
-            if (is_space(ch)):
-                print("\nFim do token por espaço")
-                input_current += 1
-                break
 
-            special = check_special_char(ch, token)
-            if(special != False):
-                if ch in {'=', '!', '<', '>', '&'}:
-                    
-                    token_class = double_special_char(ch, input)
-                    if token_class:
-                        checa_aceito(state, accept, token, token_sequence)
-                        token = ch + input.pop(0)
-                        token_sequence.append((token, token_class))
-                        input_current += 2
-                        tipo_comentario = checa_comentario(token)
-                        print("Caractere especial duplo aceito: ", token)
-                        token = ""
+            if not_comment: #ignore comments
+                if (is_space(ch)): #ends token if sees a space
 
-                else:
-                    if ch in {'+', '-', '*', '/', '(', ')', '{', '}', '[', ']', ';', ',', '.', '=', '!', '<', '>'}:
+                    print("\nFim do token por espaço")
+                    break
+
+                special = check_special_char(ch, token) #check if char is special
+
+                if(special != False):
+                    if ch in {'=', '!', '<', '>', '&'}: #check if char is a special char that can be double
                         
-                        checa_aceito(state, accept, token, token_sequence)
-                        token_sequence.append((ch, ch))
-                        print("Caractere especial aceito: ", ch)
-                        token = ""
-                        input_current += 1
-                    
+                        token_class = double_special_char(ch, input)
+                        if token_class:
+                            checa_aceito(state, accept, token, token_sequence)
+                            token = ch + input.pop(0)
+                            token_sequence.append((token, token_class))
+                            # print("Caractere especial duplo aceito: ", token)
+                            token = ""
+                            break
+                    #se for char unico
+                    checa_aceito(state, accept, token, token_sequence)
+                    token_sequence.append((ch, ch))
+                    # print("Caractere especial aceito: ", ch)
+                    token = ""
+                    break
 
-                break
-
-            if (transitionTable[state][check_char(ch)] != 3):
-                
-                token += ch
-                input_current += 1
-                # print("Estado: ", state, "Caracter: ", ch)
+            if (transitionTable[state][check_char(ch)] != 3): #if not in error state then 
                 
                 state = transitionTable[state][check_char(ch)]
-                # print("Novo estado: ", state)
+                if state not in {6, 7, 9, 10}: #if not in comment state then add to token
+                    not_comment = True
+                    token += ch
+                else:
+                    not_comment = False
+                    # print("Caractere no estado de comentario: ", ch, " estado:", transitionTable[state][check_char(ch)])
+                # print("Estado: ", state, "Caracter: ", ch)
+                
             else:
                 print("Erro")
                 break
@@ -144,4 +145,5 @@ def scanner(input ):
         print(token_sequence, "\n")
     return token_sequence
 
+Input = list("a /* nao \n eae \n nao */ oi eaeae if x==1 else x = 1 = >")
 scanner(Input)
