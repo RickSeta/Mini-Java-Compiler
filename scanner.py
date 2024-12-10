27259a85d43c0bc07631d691011b754b4a1e98ac
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-12-10 10:01:27 -0300

    especiais

--- scanner.py
+++ scanner.py
@@ -3,14 +3,58 @@ import numpy as np
 def is_space(char):
     return char in {' ', '\n', '\t', '\r', '\f'}
 
-Input = ["a","b","b","a","a"," ","e","b","a","a","b","a","a","a"]
+Input = list("aaaabba aa + a<=")
+
+T = {
+    1: {'letra': 2, 'digito': 3, }, 
+}
 
 T = {
     1: {'a': 2, 'b': 3}, 
     2: {'a': 2, 'b': 3}, 
     3: {'a': 1, 'b': 3},  
 }
+
 Accept = {1: False, 2: True, 3: False}
+
+def check_special_char(char):
+    is_special = char in {'+', '-', '*', '/', '(', ')', '{', '}', '[', ']', ';', ',', '.', '=', '!', '<', '>', '&'}
+    # print("Caractere passado: ", char)
+    if is_special:
+        # print("Caractere especial truado: ", char)
+        return True
+ 
+    return False
+def double_special_char(char, input):
+    next = input[0]
+    if char + next == '==':
+        return "=="
+    elif char + next == '!=':
+        return "!="
+    elif char + next == '<=':
+        return "<="
+    elif char + next == '>=':
+        return ">="
+    elif char + next == '&&':
+        return "and"
+    elif char + next == '/*':
+        return "/*"
+    elif char + next == '*/':
+        return "*/"
+    elif char + next == '//':
+        return "//"
+    return False
+
+def checa_aceito(state, accept, token, token_sequence):
+    if (token == ''):
+        return
+    if (accept[state]):
+        print("\nAceito")
+        print("Token: ", token,"\n")
+        token_sequence.append((token,state))
+        print(token_sequence, "\n")
+    else:
+        print("Rejeitado\n")
 
 def scanner(input, accept, transitionTable): 
     
@@ -23,6 +67,28 @@ def scanner(input, accept, transitionTable):
         while(input_current < final_index):
 
             ch = input.pop(0)
+            special = check_special_char(ch)
+            if(special != False):
+                token_class = double_special_char(ch, input)
+                if ch in {'=', '!', '<', '>', '&'} and token_class:
+                    checa_aceito(state, accept, token, token_sequence)
+                    token = ch + input.pop(0)
+                    token_sequence.append((token, token_class))
+                    input_current += 2
+                    print("Caractere especial duplo aceito: ", token)
+                    token = ""
+
+                else:
+                    if ch in {'+', '-', '*', '/', '(', ')', '{', '}', '[', ']', ';', ',', '.', '=', '!', '<', '>'}:
+                        
+                        checa_aceito(state, accept, token, token_sequence)
+                        token_sequence.append((ch, ch))
+                        print("Caractere especial aceito: ", ch)
+                        token = ""
+                        input_current += 1
+                    
+
+                break
             if (is_space(ch)):
                 print("\nFim do token por espaço")
                 input_current += 1
@@ -42,13 +108,7 @@ def scanner(input, accept, transitionTable):
 
             state = T[state][ch]
             print("Novo estado: ", state)
-        if (accept[state]):
-            print("\nAceito")
-            print("Token: ", token,"\n")
-            token_sequence.append((token,state))
-            print(token_sequence, "\n")
-        else:
-            print("Rejeitado\n")
+        checa_aceito(state, accept, token, token_sequence)
     return token_sequence
 
 scanner(Input, Accept, T)
